229d692bd873a2c77cac343d3ed2cc9464d23d57
Uy Ha <hchanuy@gmail.com>
2023-02-21 00:12:05 +0100

    add test and fix macro

--- include/peripp/macros.hpp
+++ include/peripp/macros.hpp
@@ -1,10 +1,10 @@
 #define PERIPP_CHECK(var)                                                                                              \
-  if ((var) != -1) {                                                                                                   \
+  if ((var) == -1) {                                                                                                   \
     return tl::unexpected{static_cast<std::errc>(errno)};                                                              \
   }
 
 #define PERIPP_CHECK_RETURN(var)                                                                                       \
-  if ((var) != -1) {                                                                                                   \
+  if ((var) == -1) {                                                                                                   \
     return tl::unexpected{static_cast<std::errc>(errno)};                                                              \
   }                                                                                                                    \
   return {};
--- tests/i2c.cpp
+++ tests/i2c.cpp
@@ -5,4 +5,9 @@ TEST_CASE("I2C creation", "[i2c][create]") {
   SECTION("Constructor with invalid path should throw an exception") {
     REQUIRE_THROWS_AS((peripp::I2C{"/some/random/path"}), std::system_error);
   }
+
+  SECTION("`create` static member function invoked with invalid should return a failure") {
+    auto result = peripp::I2C::create("/some/random/path");
+    REQUIRE_FALSE(result.has_value());
+  }
 }
