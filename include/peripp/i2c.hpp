62c7bf2157927fdb93577d51c313cc497f560058
Uy Ha <hchanuy@gmail.com>
2023-03-05 14:15:24 +0100

    extract common function between I2C and SMBus into a base class

--- include/peripp/i2c.hpp
+++ include/peripp/i2c.hpp
@@ -4,25 +4,45 @@
 #include <system_error>
 #include <tl/expected.hpp>
 #include <type_traits>
+
 namespace peripp {
-class I2C {
+namespace detail {
+template <typename Protocol>
+class I2CBase {
 public:
-  I2C(char const *path);
+  I2CBase(char const *path);
 
-  static auto create(char const *path) noexcept -> tl::expected<I2C, std::errc>;
+  static auto create(char const *path) noexcept -> tl::expected<Protocol, std::errc>;
 
-  I2C(I2C const &)                     = delete;
-  auto operator=(I2C const &) -> I2C & = delete;
+  I2CBase(I2CBase const &)                     = delete;
+  auto operator=(I2CBase const &) -> I2CBase & = delete;
 
-  I2C(I2C &&other) noexcept;
-  auto operator=(I2C &&other) noexcept -> I2C &;
+  I2CBase(I2CBase &&other) noexcept;
+  auto operator=(I2CBase &&other) noexcept -> I2CBase &;
 
-  ~I2C() noexcept;
+  ~I2CBase() noexcept;
 
   auto set_10_bit() noexcept -> tl::expected<void, std::errc>;
   auto set_7_bit() noexcept -> tl::expected<void, std::errc>;
 
+  auto functionality() const noexcept -> tl::expected<unsigned long, std::errc>;
+
+protected:
+  int m_handle;
+  bool is_10_bit = false;
+
+private:
+  I2CBase(int m_handle) noexcept;
+};
+} // namespace detail
+
+class I2C final : public detail::I2CBase<I2C> {
+public:
+  using I2CBase::I2CBase;
+  using I2CBase::operator=;
+
   auto send(std::uint16_t slave_address, std::span<std::uint8_t> buffer) noexcept -> tl::expected<void, std::errc>;
+
   template <typename... u8>
     requires(std::is_same_v<std::remove_cvref_t<u8>, std::uint8_t> && ...)
   auto send(std::uint16_t slave_address, u8... data) noexcept -> tl::expected<void, std::errc> {
@@ -31,12 +51,8 @@ public:
     return send(slave_address, buffer);
   }
 
-  auto functionality() const noexcept -> tl::expected<unsigned long, std::errc>;
-
-private:
-  I2C(int m_handle) noexcept;
-
-  int m_handle;
-  bool is_10_bit = false;
+  auto receive(std::uint16_t slave_address, std::span<std::uint8_t> buffer) noexcept -> tl::expected<void, std::errc>;
 };
+
+class SMBus final : public detail::I2CBase<SMBus> {};
 } // namespace peripp
--- src/i2c.cpp
+++ src/i2c.cpp
@@ -7,27 +7,33 @@
 #include <unistd.h>
 
 namespace peripp {
-I2C::I2C(char const *path)
+namespace detail {
+template <typename Protocol>
+I2CBase<Protocol>::I2CBase(char const *path)
     : m_handle{::open(path, O_RDWR)} {
   if (m_handle == -1) {
     throw std::system_error(errno, std::system_category());
   }
 }
 
-auto I2C::create(char const *path) noexcept -> tl::expected<I2C, std::errc> {
+template <typename Protocol>
+auto I2CBase<Protocol>::create(char const *path) noexcept -> tl::expected<Protocol, std::errc> {
   auto handle = ::open(path, O_RDWR);
 
   PERIPP_CHECK(handle)
 
-  return I2C{handle};
+  return Protocol{handle};
 }
 
-I2C::I2C(I2C &&other) noexcept
+template <typename Protocol>
+I2CBase<Protocol>::I2CBase(I2CBase &&other) noexcept
     : m_handle{other.m_handle} {
   other.m_handle = -1;
 }
-auto I2C::operator=(I2C &&other) noexcept -> I2C & {
-  this->~I2C();
+
+template <typename Protocol>
+auto I2CBase<Protocol>::operator=(I2CBase &&other) noexcept -> I2CBase & {
+  this->~I2CBase();
 
   m_handle       = other.m_handle;
   other.m_handle = -1;
@@ -35,7 +41,8 @@ auto I2C::operator=(I2C &&other) noexcept -> I2C & {
   return *this;
 }
 
-I2C::~I2C() noexcept {
+template <typename Protocol>
+I2CBase<Protocol>::~I2CBase() noexcept {
   if (m_handle == -1) {
     return;
   }
@@ -43,10 +50,12 @@ I2C::~I2C() noexcept {
   ::close(m_handle);
 }
 
-I2C::I2C(int handle) noexcept
+template <typename Protocol>
+I2CBase<Protocol>::I2CBase(int handle) noexcept
     : m_handle{handle} {}
 
-auto I2C::set_10_bit() noexcept -> tl::expected<void, std::errc> {
+template <typename Protocol>
+auto I2CBase<Protocol>::set_10_bit() noexcept -> tl::expected<void, std::errc> {
   auto result = ::ioctl(m_handle, I2C_TENBIT, true);
   PERIPP_CHECK(result);
 
@@ -54,7 +63,9 @@ auto I2C::set_10_bit() noexcept -> tl::expected<void, std::errc> {
 
   return {};
 }
-auto I2C::set_7_bit() noexcept -> tl::expected<void, std::errc> {
+
+template <typename Protocol>
+auto I2CBase<Protocol>::set_7_bit() noexcept -> tl::expected<void, std::errc> {
   auto result = ::ioctl(m_handle, I2C_TENBIT, false);
   PERIPP_CHECK(result);
 
@@ -62,6 +73,17 @@ auto I2C::set_7_bit() noexcept -> tl::expected<void, std::errc> {
 
   return {};
 }
+
+template <typename Protocol>
+auto I2CBase<Protocol>::functionality() const noexcept -> tl::expected<unsigned long, std::errc> {
+  unsigned long funcs;
+  auto result = ::ioctl(m_handle, I2C_FUNCS, &funcs);
+  PERIPP_CHECK(result);
+  return funcs;
+}
+} // namespace detail
+
+template class detail::I2CBase<I2C>;
 
 auto I2C::send(std::uint16_t slave_address, std::span<std::uint8_t> buffer) noexcept -> tl::expected<void, std::errc> {
   auto msg = ::i2c_msg{.addr  = slave_address,
@@ -73,12 +95,5 @@ auto I2C::send(std::uint16_t slave_address, std::span<std::uint8_t> buffer) noex
 
   auto result = ::ioctl(m_handle, I2C_RDWR, &data);
   PERIPP_CHECK_RETURN(result);
-}
-
-auto I2C::functionality() const noexcept -> tl::expected<unsigned long, std::errc> {
-  unsigned long funcs;
-  auto result = ::ioctl(m_handle, I2C_FUNCS, &funcs);
-  PERIPP_CHECK(result);
-  return funcs;
 }
 } // namespace peripp
